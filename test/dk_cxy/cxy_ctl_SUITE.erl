0133b92b3e1f2791a18bcbfdb5dda2a752d47b6f
Jay Nelson <jay@duomark.com>
2013-07-10 13:15:51 -0700

    Verify execute_task and execute_pid; Bump version to 0.9.2
    Contributor: Jay Nelson (jaynel)
    Branch: timeout
    
    Add common_test functions to verify that execute_task and execute_pid actually
    run inline or spawned functions and return the proper values.

--- test/dk_cxy/cxy_ctl_SUITE.erl
+++ test/dk_cxy/cxy_ctl_SUITE.erl
@@ -6,8 +6,12 @@
 -export([
          check_no_timer_limits/1, check_with_timer_limits/1,
          check_atom_limits/1, check_limit_errors/1,
-         check_concurrency_types/1
+         check_concurrency_types/1,
+         check_execute_task/1, check_execute_pid/1
         ]).
+
+%% Spawned functions
+-export([put_pdict/2]).
 
 -include_lib("common_test/include/ct.hrl").
 
@@ -16,7 +20,8 @@
 all() -> [
           check_no_timer_limits, check_with_timer_limits,
           check_atom_limits, check_limit_errors,
-          check_concurrency_types
+          check_concurrency_types,
+          check_execute_task, check_execute_pid
          ].
 
 -type config() :: proplists:proplist().
@@ -76,3 +81,66 @@ check_concurrency_types(_Config) ->
             || P <- [task_type, max_procs, active_procs, max_history]]
            || This_Type_Props <- Types],
     ok.
+
+%% execute_task runs a background task without feedback.
+check_execute_task(_Config) ->
+    Limits = [{ets_inline, 0, 0}, {ets_spawn, 3, 5}],
+    ok = ?TM:init(Limits),
+    check_execute_task = ets:new(check_execute_task, [public, named_table]),
+
+    try
+        %% Inline update the shared ets table...
+        ok = ?TM:execute_task(ets_inline, ets, insert_new, [check_execute_task, {joe, 5}]),
+        [{joe, 5}] = ets:lookup(check_execute_task, joe),
+        ok = ?TM:execute_task(ets_inline, ets, insert, [check_execute_task, {joe, 7}]),
+        [{joe, 7}] = ets:lookup(check_execute_task, joe),
+        true = ets:delete(check_execute_task, joe),
+
+        %% Spawn update the shared ets table.
+        ok = ?TM:execute_task(ets_spawn, ets, insert_new, [check_execute_task, {joe, 4}]),
+        erlang:yield(),
+        [{joe, 4}] = ets:lookup(check_execute_task, joe),
+        ok = ?TM:execute_task(ets_spawn, ets, insert, [check_execute_task, {joe, 6}]),
+        erlang:yield(),
+        [{joe, 6}] = ets:lookup(check_execute_task, joe),
+        true = ets:delete(check_execute_task, joe)
+    after true = ets:delete(check_execute_task)
+    end,
+
+    ok.
+
+%% execute_pid runs a task with a return value of the Pid or {inline, Result}.
+check_execute_pid(_Config) ->
+    Limits = [{pdict_inline, 0, 0}, {pdict_spawn, 3, 5}],
+    ok = ?TM:init(Limits),
+    
+    %% When inline, update our process dictionary...
+    Old_Joe = erase(joe),
+    try
+        {inline, undefined} = ?TM:execute_pid(pdict_inline, erlang, put, [joe, 5]),
+        5 = get(joe),
+        {inline, 5} = ?TM:execute_pid(pdict_inline, erlang, put, [joe, 7]),
+        7 = get(joe)
+    after put(joe, Old_Joe)
+    end,
+
+    %% When spawned, it affects a new process dictionary, not ours.
+    Old_Joe = erase(joe),
+    try
+        undefined = get(joe),
+        New_Pid = ?TM:execute_pid(pdict_spawn, ?MODULE, put_pdict, [joe, 5]),
+        false = New_Pid =:= self(),
+        New_Pid ! {self(), get_pdict, joe},
+        undefined = get(joe),
+        ok = receive {get_pdict, New_Pid, 5} -> ok
+             after 100 -> timeout
+             end
+    after put(joe, Old_Joe)
+    end,
+
+    ok.
+
+put_pdict(Key, Value) ->
+    put(Key, Value),
+    receive {From, get_pdict, Key} -> From ! {get_pdict, self(), get(Key)} end.
+            
