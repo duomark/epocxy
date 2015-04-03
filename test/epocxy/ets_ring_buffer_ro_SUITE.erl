e1dc2753b1f81b576f42f594abf9800adf14f319
Jay Nelson <jay@duomark.com>
2015-04-03 12:50:16 -0700

    In progress updates to PropEr tests for rings

--- test/epocxy/ets_ring_buffer_ro_SUITE.erl
+++ test/epocxy/ets_ring_buffer_ro_SUITE.erl
@@ -22,7 +22,7 @@
         ]).
 
 -export([
-         proper_check_create/1
+         proper_new_ring_is_empty/1
         ]).
 
 -include("epocxy_common_test.hrl").
@@ -33,7 +33,7 @@
 -spec all() -> [test_case() | {group, test_group()}].
 
 all() -> [
-          proper_check_create     % Ensure we can create new rings
+          proper_new_ring_is_empty     % Ensure we can create new rings
          ].
 
 %% -spec groups() -> [test_group(), [sequence], [test_case() | {group, test_group()}]].
@@ -70,9 +70,9 @@ end_per_testcase  (_Test_Case, Config) -> Config.
 gen_ring_names() ->
     ?LET(Ring_Names, list(atom()), Ring_Names).
 
-%% Validate any atom can be used as a ring_name and list will report properly.
+%% Validate any atom can be used as a ring_name and ring is empty by default.
 -spec proper_check_create(config()) -> ok.
-proper_check_create(_Config) ->
+proper_new_ring_is_empty(_Config) ->
     {ok, Sup_Pid} = epocxy_sup:start_link(),
     Fsm_Pid       = whereis(epocxy_ets_fsm),
 
@@ -80,7 +80,7 @@ proper_check_create(_Config) ->
     Test_Ring_Name = ?FORALL(Names, gen_ring_names(),
                              case [N || N <- Names, N =/= ''] of
                                  []         -> true;
-                                 Ring_Names -> check_create_test(Ring_Names, Sup_Pid, Fsm_Pid)
+                                 Ring_Names -> check_empty_create(Ring_Names, Sup_Pid, Fsm_Pid)
                              end),
     true = proper:quickcheck(Test_Ring_Name, ?PQ_NUM(5)),
     ct:comment("Successfully tested atoms as ring_names"),
@@ -89,16 +89,14 @@ proper_check_create(_Config) ->
     cleanup(Sup_Pid, Fsm_Pid),
     ok.
                               
-check_create_test(Ring_Names, Sup_Pid, Fsm_Pid) ->
+check_empty_create(Ring_Names, Sup_Pid, Fsm_Pid) ->
     %% No rings exist yet...
     [] = ?TM:list(),
     [] = ?TM:list(missing_ring),
 
     ct:comment ("Testing ring_names ~p", [Ring_Names]),
     ct:log     ("Testing ring_names ~p", [Ring_Names]),
-    Num_Rings   = length(Ring_Names),
-    Exp_Counts  = lists:duplicate(Num_Rings, 0),
-    Exp_Counts  = [check_one_ring(R) || R <- Ring_Names],
+    [check_empty_ring(R) || R <- Ring_Names],
 
     ct:comment("Deleting all rings"),
     Exp_Deletes = lists:duplicate(Num_Rings, true),
@@ -107,7 +105,7 @@ check_create_test(Ring_Names, Sup_Pid, Fsm_Pid) ->
 
     true.
 
-check_one_ring(Ring_Name) ->
+check_empty_ring(Ring_Name) ->
     true = ?TM:create    (Ring_Name),
     0    = ?TM:ring_size (Ring_Name).
 
