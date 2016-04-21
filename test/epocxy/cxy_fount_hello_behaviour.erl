492f1f2a924f253126a78cd2f9400f5be52048d3
Jay Nelson <jay@duomark.com>
2016-04-21 16:45:39 -0700

    Make cxy_regulator test run again

--- test/epocxy/cxy_fount_hello_behaviour.erl
+++ test/epocxy/cxy_fount_hello_behaviour.erl
@@ -14,14 +14,21 @@
 -behaviour(cxy_fount).
 
 %% Behaviour API
--export([init/0, start_pid/1, send_msg/2]).
+-export([init/1, start_pid/2, send_msg/2]).
 
 %% For testing only
 -export([say_to/2]).
 
--include("cxy_fount.hrl").
+-type fount   () :: cxy_fount:fount_ref().
+-type stamp   () :: erlang:timestamp().
+-type pid_msg () :: any().
 
-init() ->
+-spec init      (any())             -> stamp().
+-spec start_pid (fount(),  stamp()) -> pid()  | {error, Reason::any()}.
+-spec send_msg  (Worker, pid_msg()) -> Worker | {error, Reason::any()}
+                                           when Worker :: pid().
+
+init(_) ->
     os:timestamp().
     
 start_pid(Fount, Started) ->
@@ -34,14 +41,14 @@ send_msg(Worker, Msg) ->
 %% Idle workers may wait a while before being used in a test.
 wait_for_hello(Started) ->
     receive
-        {Ref, From, hello} -> reply(Started, From, Ref, goodbye);
-        Unexpected         -> reply(Started, From, Ref, {unexpected, Unexpected})
+        {Ref, From,      hello} -> reply(Started, From, Ref, goodbye);
+        {Ref, From, Unexpected} -> reply(Started, From, Ref, {unexpected, Unexpected})
     after 30000 -> wait_for_hello_timeout
     end.
 
 reply(Started, From, Ref, Msg) ->
     Elapsed = timer:now_diff(os:timestamp(), Started),
-    From ! {Ref, Msg, now(), {elapsed, Elapsed}};
+    From ! {Ref, Msg, now(), {elapsed, Elapsed}}.
 
 %% Just verify the goodbye response comes after saying hello.
 say_to(Worker, Msg) ->
